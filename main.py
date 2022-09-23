90943136d4dffe0642ec83c2788a88fff254066e
DanielJeffrey-2 <113033888+DanielJeffrey-2@users.noreply.github.com>
2022-09-23 10:00:21 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# Add your Python code here. E.g.
-from microbit import *
-
-
-while True:
-    display.scroll('Hello, World!')
-    display.show(Image.HEART)
-    sleep(2000)
+def onEvery_interval():
+    pass
+loops.every_interval(500, onEvery_interval)
+basic.show_string("Hello, World!")
+basic.show_icon(IconNames.HEART)
--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+loops.everyInterval(500, function onEvery_interval() {
+    
+})
+basic.showString("Hello, World!")
+basic.showIcon(IconNames.Heart)
